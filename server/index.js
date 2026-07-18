548f526f2f430b26652314aaf5a0fa6d848faf7f
Vedika Singh <vedikasingh2445@gmail.com>
2026-07-18 17:17:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewiq-ai-client-ivk3.onrender.com",
     credentials: true
 }))
 
